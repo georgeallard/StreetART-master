7e904cd71e0bad17e074a4776c7f9406013b7ae6
georgeallard <i7259568@bournemouth.ac.uk>
2017-06-04 17:15:41 +0100

    commit

--- StreetART/MapPinViewController.swift
+++ StreetART/MapPinViewController.swift
@@ -15,6 +15,7 @@ import Firebase
 class MapPinViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var addArt: UIBarButtonItem!
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var ref = FIRDatabase.database().reference().child("art")
@@ -27,35 +28,34 @@ class MapPinViewController: UIViewController, MKMapViewDelegate, CLLocationManag
     
     let locationManager = CLLocationManager()
     
+    
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let logo = UIImage(named: "streetARNAV")
+        
         let imageView = UIImageView(image: logo)
+        
         self.navigationItem.titleView = imageView
 
         mapView.delegate = self
-        locationManager.requestAlwaysAuthorization()
-        locationManager.delegate = self
-        locationManager.startUpdatingLocation()
-        mapView.userTrackingMode = .follow
         
-        loadCustomLocations()
+        locationManager.requestAlwaysAuthorization()
+        
+        locationManager.delegate = self
+        
+        locationManager.startUpdatingLocation()
+        
+        mapView.userTrackingMode = .follow
         
         let ArtIcon = MKPointAnnotation()
         
         mapView.addAnnotation(ArtIcon)
         
-        
+         loadCustomLocations()
     }
- 
-    override func viewDidAppear(_ animated: Bool) {
-          //  self.loadCustomLocations()
-    }
-        
-
 
         
     @IBAction func addArt_TouchUpInside(_ sender: Any) {
@@ -123,8 +123,7 @@ class MapPinViewController: UIViewController, MKMapViewDelegate, CLLocationManag
             textField.textColor = UIColor.black
         }
 
-
-
+        
         alertController.addAction(saveAction)
         alertController.addAction(cancelAction)
         alertController.view.tintColor = UIColor.black  // change text color of the buttons
@@ -133,101 +132,46 @@ class MapPinViewController: UIViewController, MKMapViewDelegate, CLLocationManag
         
         self.present(alertController, animated: true, completion: nil)
         
-        
-        
-        
-        
-        
-    }
-    
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        guard !(annotation is MKUserLocation) else {
-            return nil
-        }
-        
-        let annotationIdentifier = "Identifier"
-        var annotationView: MKAnnotationView?
-        if let dequeuedAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) {
-            annotationView = dequeuedAnnotationView
-            annotationView?.annotation = annotation
-        }
-        else {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        
-        if let annotationView = annotationView {
-            
-            annotationView.canShowCallout = true
-            annotationView.image = UIImage(named: "MapAnnotation")
-        }
-        return annotationView
-        
-        
 
     }
     
-    
-    
-    
-    
-//    func CreateAnnotation(artDrops: art) {
-//        
-//        
-//        let point = artDrops()
-//        
-//        point.name = art.name
-//        
-//        point.coordinate = art.coordinate
-//        
-//        point.location = art.location
-//        
-//        point.type = art.type
-//        
-//        mapView.addAnnotation(point as! MKAnnotation)
-//        
-//        mapView.selectAnnotation(point as! MKAnnotation, animated: true)
-//        
-//    }
 //    
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        guard !(annotation is MKUserLocation) else {
+//            return nil
+//        }
+//        
+//        let annotationIdentifier = "Identifier"
+//        var annotationView: MKAnnotationView?
+//        if let dequeuedAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) {
+//            annotationView = dequeuedAnnotationView
+//            annotationView?.annotation = annotation
+//        }
+//        else {
+//            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+//            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+//        }
+//        
+//        if let annotationView = annotationView {
+//            
+//            annotationView.canShowCallout = true
+//            annotationView.image = UIImage(named: "MapAnnotation")
+//        }
+//        return annotationView
+//        
+//        
 //
-//    
-////    func loadCustomLocations() {
-////        
-////        ref.observe(.childAdded, with: { snapshot in
-////            
-////            let lat = (snapshot.value as AnyObject!)!["lat"] as! String!
-////            let lng = (snapshot.value as AnyObject!)!["lg"] as! String!
-////            let name = (snapshot.value as AnyObject!)!["name"] as! String!
-////            let type = (snapshot.value as AnyObject!)!["type"] as! String!
-////            let location = (snapshot.value as AnyObject!)!["location"] as! String!
-////            
-////            let annotation = addAnno(name: name!, coordinate: CLLocationCoordinate2D(latitude: lat!, longitude: lng!), type: type!, location: location!)
-////            
-////            //my annotationView is my custom class for annotations.
-////            self.mapView.addAnnotation(self.addAnno as MKAnnotation)
-////            
-////            print(snapshot)
-////            
-////        })}
-////        
-////        
-////        
-////        
-////        
-////        
-////    }
-//
-//    
+
     
+    
+ 
     func loadCustomLocations() {
         
         FIRDatabase.database().reference(withPath: "art")
         
-        let artLoc = FIRDatabase.database().reference(withPath: "art")
+        let artPin = FIRDatabase.database().reference(withPath: "art")
         
-        artLoc.observe(.value, with: { snapshot in
+        artPin.observe(.value, with: { snapshot in
             
             
             for item in snapshot.children {
@@ -238,11 +182,20 @@ class MapPinViewController: UIViewController, MKMapViewDelegate, CLLocationManag
                 print(snapshot)
                 
                 self.artDrops.append(ref)
-//
-//                self.CreateAnnotation(artDrops: art)
+                
+         //       self.makeAnnotation(artDrops: art)
+                
                 
             }
         })
     }
-
+    
 }
+
+    
+
+    
+    
+    
+    
+    
